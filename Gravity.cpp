d136b6f049a4b8a7050612aac77d21bb00b146a5
WiktorK02 <123249470+WiktorK02@users.noreply.github.com>
2023-02-23 22:55:08 +0100

    Add files via upload

---
+++ Gravity.cpp
@@ -0,0 +1,71 @@
+﻿#include  <SFML\Graphics.hpp>
+#include <iostream>
+#include "Particle.cpp"
+#include "Strength.cpp"
+
+#define SCREEN_HIGHT 960
+#define SCREEN_WIDTH 1200
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HIGHT), "title");
+    window.setFramerateLimit(60);
+    
+    //CRICLE declaration
+    Grav_Strength grav_strength;
+    Particle mass1(150, 400, 60, 0, 0, 50, 0, 0);//posx, posy, radius, velx, vely, accx, accy
+    Particle mass2(600, 500, 60, 0.6, 2, 60, 89, -50);//posx, posy, radius, velx, vely, accx, accy
+    sf::Event event;
+    
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+
+        while (window.pollEvent(event)) {
+
+            switch (event.type) {
+
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;
+            }
+
+        }
+        
+        grav_strength.distance(mass1, mass2);
+        grav_strength.force(mass1, mass2);
+        grav_strength.force_mass1_x(mass1);
+        float dt = clock.restart().asSeconds();
+
+        //std::cout << grav_strength.get_force_mass1_x()<< std::endl;
+        //render
+        window.clear();//clear old frames
+
+        //draw 
+        mass1.render(window);
+        mass2.render(window);
+        
+        //line
+        sf::Vertex line[] = { sf::Vertex(sf::Vector2f(mass1.get_pos().x + mass1.get_radius(), mass1.get_pos().y + mass1.get_radius())), sf::Vertex(sf::Vector2f(mass2.get_pos().x + mass1.get_radius(), mass2.get_pos().y + mass1.get_radius())) };
+        window.draw(line, 2, sf::Lines);
+
+        mass1.velocity(dt);
+        mass2.velocity(dt);
+        mass1.acceleration(dt);
+        mass2.acceleration(dt);
+        //mass1.force(grav_strength);
+        //mass2.force(grav_strength);
+        mass1.collision();
+        mass2.collision();
+
+        window.display();
+
+
+    }
+
+    return 0;
+}
---
+++ Strength.cpp
@@ -0,0 +1,63 @@
+#include <cmath>
+#include <iostream>
+#include  <SFML\Graphics.hpp>
+#define SCREEN_HIGHT 960
+#define SCREEN_WIDTH 1200
+#define G_Constant 6.6743 * pow(10, -11)
+
+class Grav_Strength
+{
+public:
+	Grav_Strength();
+	float get_distance(Particle& mass1, Particle& mass2) {
+		x = mass1.get_pos().x + mass1.get_radius() - mass2.get_pos().x + mass2.get_radius();
+		y = mass1.get_pos().y + mass1.get_radius() - mass2.get_pos().y + mass2.get_radius();
+		r = sqrt(pow(x, 2) + pow(y, 2));
+		return r;
+	}
+	float get_force(Particle& mass1, Particle& mass2) {
+		return (G_Constant * mass1.get_mass() * mass2.get_mass() / pow(r, 2));
+	}
+	float get_force_mass1_x() {
+		return f_mass1_x;
+	}
+	float get_force_mass1_y() {
+		return f_mass1_y;
+	}
+	//return distance beetwen two masess
+	void distance(Particle& mass1, Particle& mass2) {
+		this->x = mass1.get_pos().x + mass1.get_radius() - mass2.get_pos().x + mass2.get_radius();
+		this->y = mass1.get_pos().y + mass1.get_radius() - mass2.get_pos().y + mass2.get_radius();
+		this->r = sqrt(pow(x, 2) + pow(y, 2));
+	}
+
+	void force(Particle &mass1, Particle &mass2) {
+		this->f = (mass1.get_mass() * mass2.get_mass()) / pow(r, 2);
+	}
+	void force_mass1_x(Particle& mass1) {
+		this->f_mass1_x = f * x / r;
+	}
+	void force_mass1_y(Particle& mass1) {
+		this->f_mass1_y = f * y / r;
+	}
+	void force_mass2_x(Particle& mass2) {
+		this->f_mass1_x = f * x / r;
+	}
+	void force_mass2_y(Particle& mass2) {
+		this->f_mass2_y = f * y / r;
+	}
+	
+	~Grav_Strength();
+
+private:
+	float x, y, r;
+	float f, f_mass1_x, f_mass2_x, f_mass1_y, f_mass2_y;
+};
+
+Grav_Strength::Grav_Strength()
+{
+}
+
+Grav_Strength::~Grav_Strength()
+{
+}
